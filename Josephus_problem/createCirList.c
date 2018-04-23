b752e991f7d241a7e9bb05e79cdbd968c741645c
Rhys <811437508@qq.com>
2018-04-23 14:57:36 +0800

    Update 2

--- Josephus_problem/createCirList.c
+++ Josephus_problem/createCirList.c
@@ -7,31 +7,28 @@ extern void createCirList(CirList *L, int n)
     CirList phead;
     *L = (CirList)malloc(sizeof(CirNode));
     (*L)->next = *L;
-    phead = *L;
+    phead = *L;    //构建循环链表
     int i;
 
     for ( i = 0 ; i<n ; i++ )
     {
-        if ( i == 0)
+        if ( i == 0) //第一个节点
         {
-           // printf("enter the %d people's code.\n",i+1);
-           //scanf("%d",&(*L)->data);
-           (*L)->data = i+5 ;
-
-            (*L)->number = i+1;
+           printf("enter the %d people's code.\n",i+1);
+           scanf("%d",&(*L)->data);
+           (*L)->number = i+1;
 
         }
-        else
+        else。  //第n个节点
         {
-            newptr = (CirList)malloc(sizeof(CirNode));
-            //printf("enter the %d people's code.\n",i+1);
-            //scanf("%d",&newptr->data);
-            newptr->data = i+5;
+            newptr = (CirList)malloc(sizeof(CirNode));   创建节点
+            printf("enter the %d people's code.\n",i+1);
+            scanf("%d",&newptr->data);
             newptr->number = i+1;
-            while( phead->next != *L)
+            while( phead->next != *L)。  //找到头节点的前一个节点
             phead = phead->next;
 
-            newptr->next = phead->next;
+            newptr->next = phead->next;   //插入节点
             phead->next = newptr;
         }
 
